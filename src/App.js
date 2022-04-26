7ef2938bdfa98511c327fece566bb7011341df3a
Aniket <aniketsaste96@gmail.com>
2022-04-26 18:44:52 +0530

    travel advisor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from "./components/Header/Header"
+import List from "./components/List/List"
+import Map from "./components/Map/Map"
+
+import { CssBaseline, Grid } from "@material-ui/core"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={3} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={3} md={4}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
